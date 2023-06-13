231234f230edbdd1fe39cdc32b849ededee37a7e
KingKyoTrue <tanadicoding@gmail.com>
2023-06-14 00:20:21 +0700

    update topics

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -5,13 +5,19 @@ import blog2 from "./blog2.jpg";
 import blog3 from "./blog3.jpg";
 import closeIcon from "./close-icon.svg";
 import daw from "./daw.png";
-import facebookLogo from "./facebook-logo.svg";
+import facebookLogo from "./facebook-icon.svg";
 import heroImage from "./hero-image.jpg";
 import instagramLogo from "./instagram-logo.svg";
 import logo from "./logo.png";
 import menuIcon from "./menu-icon.svg";
 import mixing from "./mixing.png";
 import studentIcon from "./student-icon.svg";
+import topic1 from "./topic1.png";
+import topic2 from "./topic2.png";
+import topic3 from "./topic3.png";
+import topic4 from "./topic4.png";
+import topic5 from "./topic5.png";
+import topic6 from "./topic6.png";
 import testimonial1 from "./testimonial1.png";
 import testimonial2 from "./testimonial2.png";
 import testimonial3 from "./testimonial3.png";
@@ -34,6 +40,12 @@ export {
   menuIcon,
   mixing,
   studentIcon,
+  topic1,
+  topic2,
+  topic3,
+  topic4,
+  topic5,
+  topic6,
   testimonial1,
   testimonial2,
   testimonial3,
--- src/components/Topics/Topics.jsx
+++ src/components/Topics/Topics.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from "react";
 import "./Topics.css";
+import {
+  topic1,
+  topic2,
+  topic3,
+  topic4,
+  topic5,
+  topic6,
+  bgElement2,
+} from "../../assets/images";
 
 const Topics = () => {
-  return (
-    <div>Topics</div>
-  )
-}
+  const [currentImage, setCurrentImage] = useState(topic1);
 
-export default Topics
+  return (
+    <section id="topics" className="black">
+      <div className="wrapper">
+        <h2>What will you learn?</h2>
+        <div className="content-container">
+          <ul className="topics-list">
+            <li onMouseEnter={() => setCurrentImage(topic1)}>
+              What are frequencies?
+            </li>
+            <li onMouseEnter={() => setCurrentImage(topic2)}>Using the DAW</li>
+            <li onMouseEnter={() => setCurrentImage(topic3)}>
+              Vocals Processing
+            </li>
+            <li onMouseEnter={() => setCurrentImage(topic4)}>Mixing</li>
+            <li onMouseEnter={() => setCurrentImage(topic5)}>Mixing Console</li>
+            <li onMouseEnter={() => setCurrentImage(topic6)}>Mastering</li>
+          </ul>
+
+          <div className="topic-image">
+            <img src={currentImage}></img>
+          </div>
+        </div>
+        <img src={bgElement2} className="bg-element-2"></img>
+      </div>
+    </section>
+  );
+};
+
+export default Topics;
