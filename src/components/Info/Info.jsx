396a45a2a41e7dc819b5a035902c6de1363cf3b6
KingKyoTrue <tanadicoding@gmail.com>
2023-06-14 00:40:41 +0700

    update info

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React from "react";
 import "./Info.css";
+import { studentIcon, videoIcon, bgElement1 } from "../../assets/images";
 
 const Info = () => {
   return (
-    <div>Info</div>
-  )
-}
+    <section id="info" className="dark-gray">
+      <div className="wrapper">
+        <div className="content-container">
+          <div className="info-content">
+            <img src={studentIcon} />
+            <div className="amount">23.000+</div>
+            <div className="type">Students</div>
+          </div>
 
-export default Info
+          <div className="info-content">
+            <img src={videoIcon} />
+            <div className="amount">26 Hrs</div>
+            <div className="type">Video Content</div>
+          </div>
+        </div>
+
+        <img className="bg-element-1" src={bgElement1} />
+      </div>
+    </section>
+  );
+};
+
+export default Info;
